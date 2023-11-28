69a8ca947553bb67515ed39ee70fd55362071e8a
Andreas Buis <abuis@splunk.com>
2023-11-28 20:57:37 +0000

    ss

--- detonate_file.py
+++ detonate_file.py
@@ -66,14 +66,14 @@ def vt_detonate_file(action=None, success=None, container=None, results=None, ha
     ## Custom Code End
     ################################################################################
 
-    phantom.act("detonate file", parameters=parameters, name="vt_detonate_file", assets=["virustotal"], callback=get_right_artifact)
+    phantom.act("detonate file", parameters=parameters, name="vt_detonate_file", assets=["virustotal"], callback=filter_get_right_artifact)
 
     return
 
 
 @phantom.playbook_block()
-def get_right_artifact(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("get_right_artifact() called")
+def filter_get_right_artifact(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("filter_get_right_artifact() called")
 
     # collect filtered artifact ids and results for 'if' condition 1
     matched_artifacts_1, matched_results_1 = phantom.condition(
@@ -81,41 +81,12 @@ def get_right_artifact(action=None, success=None, container=None, results=None, 
         conditions=[
             ["filtered-data:filter_unpacked_files:condition_1:artifact:*.cef.vaultId", "==", "vt_detonate_file:action_result.parameter.vault_id"]
         ],
-        name="get_right_artifact:condition_1",
+        name="filter_get_right_artifact:condition_1",
         delimiter=None)
 
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_1 or matched_results_1:
-        format_2(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
-
-    return
-
-
-@phantom.playbook_block()
-def format_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("format_2() called")
-
-    template = """%%\n{{ \"cef\": {{\n\"vaultId\": \"{0}\",\n\"scan_id\": \"{1}\"\n}} }}\n%%\n"""
-
-    # parameter list for template variable replacement
-    parameters = [
-        "filtered-data:filter_unpacked_files:condition_1:artifact:*.cef.vaultId",
-        "filtered-data:get_right_artifact:condition_1:vt_detonate_file:action_result.summary.scan_id"
-    ]
-
-    ################################################################################
-    ## Custom Code Start
-    ################################################################################
-
-    # Write your custom code here...
-
-    ################################################################################
-    ## Custom Code End
-    ################################################################################
-
-    phantom.format(container=container, template=template, parameters=parameters, name="format_2")
-
-    debug_2(container=container)
+        format_tags(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
 
     return
 
@@ -152,6 +123,75 @@ def debug_2(action=None, success=None, container=None, results=None, handle=None
     ################################################################################
 
     phantom.custom_function(custom_function="community/debug", parameters=parameters, name="debug_2")
+
+    return
+
+
+@phantom.playbook_block()
+def update_artifact(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("update_artifact() called")
+
+    filtered_artifact_0_data_filter_get_right_artifact = phantom.collect2(container=container, datapath=["filtered-data:filter_get_right_artifact:condition_1:artifact:*.id","filtered-data:filter_get_right_artifact:condition_1:artifact:*.id"])
+    filtered_result_0_data_filter_get_right_artifact = phantom.collect2(container=container, datapath=["filtered-data:filter_get_right_artifact:condition_1:vt_detonate_file:action_result.summary.scan_id"])
+    format_tags = phantom.get_format_data(name="format_tags")
+
+    parameters = []
+
+    # build parameters list for 'update_artifact' call
+    for filtered_artifact_0_item_filter_get_right_artifact in filtered_artifact_0_data_filter_get_right_artifact:
+        for filtered_result_0_item_filter_get_right_artifact in filtered_result_0_data_filter_get_right_artifact:
+            parameters.append({
+                "artifact_id": filtered_artifact_0_item_filter_get_right_artifact[0],
+                "name": None,
+                "label": None,
+                "severity": None,
+                "cef_field": "scan_id",
+                "cef_value": filtered_result_0_item_filter_get_right_artifact[0],
+                "cef_data_type": None,
+                "tags": format_tags,
+                "overwrite_tags": None,
+                "input_json": None,
+            })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.custom_function(custom_function="community/artifact_update", parameters=parameters, name="update_artifact")
+
+    return
+
+
+@phantom.playbook_block()
+def format_tags(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("format_tags() called")
+
+    template = """{0}, pending_scan"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "filtered-data:filter_get_right_artifact:condition_1:artifact:*.tags"
+    ]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.format(container=container, template=template, parameters=parameters, name="format_tags")
+
+    update_artifact(container=container)
 
     return
 
