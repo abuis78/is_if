406a54cb2a062b3cb47e470e5e7f8319382e7e86
Andreas Buis <abuis@splunk.com>
2023-11-28 22:23:09 +0000

    ddd

--- final_check.py
+++ final_check.py
@@ -12,9 +12,42 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-
+    # call 'code_1' block
+    code_1(container=container)
 
     return
+
+@phantom.playbook_block()
+def code_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("code_1() called")
+
+    id_value = container.get("id", None)
+
+    code_1__count = None
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+    # artifact?_filter_container=72293&_filter_name__icontains="Vault%20Artifact:"&_exclude_severity="low"
+
+    
+    u_filter = '?_filter_container='+ str(id_value) +'&_filter_name__icontains="Vault Artifact:"&_exclude_severity="low"'
+    
+    url = phantom.build_phantom_rest_url('indicator')
+    url_filter = url + u_filter
+    r = phantom.requests.get(url,verify=False)
+    data = r.json()
+    phantom.debug(data)
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.save_run_data(key="code_1:count", value=json.dumps(code_1__count))
+
+    return
+
 
 @phantom.playbook_block()
 def on_finish(container, summary):
