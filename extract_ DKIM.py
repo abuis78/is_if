9890db006f7b5ee2efb291c826adf14139d1a2d3
Andreas Buis <abuis@splunk.com>
2023-11-28 15:17:49 +0000

    sd

--- extract_ DKIM.py
+++ extract_ DKIM.py
@@ -52,11 +52,79 @@ def extract_dkim_signatur(action=None, success=None, container=None, results=Non
         #phantom.debug(f"{key}: {value}")
         
     phantom.debug(cef_json)
+    extract_dkim_signatur__dkim_json = cef_json
     ################################################################################
     ## Custom Code End
     ################################################################################
 
     phantom.save_run_data(key="extract_dkim_signatur:dkim_json", value=json.dumps(extract_dkim_signatur__dkim_json))
+
+    format_json_for_create_artifact(container=container)
+
+    return
+
+
+@phantom.playbook_block()
+def format_json_for_create_artifact(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("format_json_for_create_artifact() called")
+
+    template = """{{ \"cef\": {{ {0} }} }}"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "extract_dkim_signatur:custom_function:dkim_json"
+    ]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.format(container=container, template=template, parameters=parameters, name="format_json_for_create_artifact")
+
+    artifact_create_1(container=container)
+
+    return
+
+
+@phantom.playbook_block()
+def artifact_create_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("artifact_create_1() called")
+
+    id_value = container.get("id", None)
+    format_json_for_create_artifact = phantom.get_format_data(name="format_json_for_create_artifact")
+
+    parameters = []
+
+    parameters.append({
+        "container": id_value,
+        "name": "DKIM-Signatur",
+        "label": None,
+        "severity": None,
+        "cef_field": None,
+        "cef_value": None,
+        "cef_data_type": None,
+        "tags": None,
+        "run_automation": None,
+        "input_json": format_json_for_create_artifact,
+    })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.custom_function(custom_function="community/artifact_create", parameters=parameters, name="artifact_create_1")
 
     return
 
