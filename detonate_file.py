29a827988f2c682e62f9d4d447970046d28ae12d
Andreas Buis <abuis@splunk.com>
2023-11-28 21:22:00 +0000

    ddd

--- detonate_file.py
+++ detonate_file.py
@@ -52,7 +52,7 @@ def vt_detonate_file(action=None, success=None, container=None, results=None, ha
         if filtered_artifact_0_item_filter_unpacked_files[0] is not None:
             parameters.append({
                 "vault_id": filtered_artifact_0_item_filter_unpacked_files[0],
-                "wait_time": 5,
+                "wait_time": 1,
                 "context": {'artifact_id': filtered_artifact_0_item_filter_unpacked_files[1]},
             })
 
@@ -66,63 +66,7 @@ def vt_detonate_file(action=None, success=None, container=None, results=None, ha
     ## Custom Code End
     ################################################################################
 
-    phantom.act("detonate file", parameters=parameters, name="vt_detonate_file", assets=["virustotal"], callback=update_artifact)
-
-    return
-
-
-@phantom.playbook_block()
-def filter_get_right_artifact_vaultid(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("filter_get_right_artifact_vaultid() called")
-
-    # collect filtered artifact ids and results for 'if' condition 1
-    matched_artifacts_1, matched_results_1 = phantom.condition(
-        container=container,
-        conditions=[
-            ["vt_detonate_file:action_result.parameter.vault_id", "==", "filtered-data:filter_unpacked_files:condition_1:artifact:*.cef.vaultId"]
-        ],
-        name="filter_get_right_artifact_vaultid:condition_1",
-        delimiter=None)
-
-    # call connected blocks if filtered artifacts or results
-    if matched_artifacts_1 or matched_results_1:
-        pass
-
-    return
-
-
-@phantom.playbook_block()
-def debug_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("debug_2() called")
-
-    format_2 = phantom.get_format_data(name="format_2")
-
-    parameters = []
-
-    parameters.append({
-        "input_1": format_2,
-        "input_2": None,
-        "input_3": None,
-        "input_4": None,
-        "input_5": None,
-        "input_6": None,
-        "input_7": None,
-        "input_8": None,
-        "input_9": None,
-        "input_10": None,
-    })
-
-    ################################################################################
-    ## Custom Code Start
-    ################################################################################
-
-    # Write your custom code here...
-
-    ################################################################################
-    ## Custom Code End
-    ################################################################################
-
-    phantom.custom_function(custom_function="community/debug", parameters=parameters, name="debug_2")
+    phantom.act("detonate file", parameters=parameters, name="vt_detonate_file", assets=["virustotal"], callback=format_tags)
 
     return
 
@@ -174,7 +118,7 @@ def format_tags(action=None, success=None, container=None, results=None, handle=
 
     # parameter list for template variable replacement
     parameters = [
-        "filtered-data:filter_get_right_artifact_vaultid:condition_1:artifact:*.tags.0"
+        "filtered-data:filter_unpacked_files:condition_1:artifact:*.tags.0"
     ]
 
     ################################################################################
@@ -189,25 +133,7 @@ def format_tags(action=None, success=None, container=None, results=None, handle=
 
     phantom.format(container=container, template=template, parameters=parameters, name="format_tags")
 
-    return
-
-
-@phantom.playbook_block()
-def filter_get_artifact_id(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("filter_get_artifact_id() called")
-
-    # collect filtered artifact ids and results for 'if' condition 1
-    matched_artifacts_1, matched_results_1 = phantom.condition(
-        container=container,
-        conditions=[
-            ["filtered-data:filter_get_right_artifact_vaultid:condition_1:artifact:*.id", "==", "filtered-data:filter_unpacked_files:condition_1:artifact:*.id"]
-        ],
-        name="filter_get_artifact_id:condition_1",
-        delimiter=None)
-
-    # call connected blocks if filtered artifacts or results
-    if matched_artifacts_1 or matched_results_1:
-        format_tags(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+    update_artifact(container=container)
 
     return
 
