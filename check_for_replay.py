ab2523e0bd18ba5e132015d45f79dbc0624c71a4
Andreas Buis <abuis@splunk.com>
2023-11-29 13:23:58 +0000

    sss

--- check_for_replay.py
+++ check_for_replay.py
@@ -45,7 +45,7 @@ def check_prompt_status(action=None, success=None, container=None, results=None,
     ## Custom Code End
     ################################################################################
 
-    phantom.act("check status", parameters=parameters, name="check_prompt_status", assets=["urlprompt"], callback=decision_1)
+    phantom.act("check status", parameters=parameters, name="check_prompt_status", assets=["urlprompt"], callback=decission_prompt)
 
     return
 
@@ -71,8 +71,8 @@ def filter_url_prompt_artifact(action=None, success=None, container=None, result
 
 
 @phantom.playbook_block()
-def decision_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("decision_1() called")
+def decission_prompt(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("decission_prompt() called")
 
     # check for 'if' condition 1
     found_match_1 = phantom.decision(
@@ -86,6 +86,9 @@ def decision_1(action=None, success=None, container=None, results=None, handle=N
     if found_match_1:
         filter_file_artifact_with_tag_pwd_protected(action=action, success=success, container=container, results=results, handle=handle)
         return
+
+    # check for 'else' condition 2
+    get_scheduler_artifact(action=action, success=success, container=container, results=results, handle=handle)
 
     return
 
@@ -146,33 +149,16 @@ def unzip_file_2(action=None, success=None, container=None, results=None, handle
     ## Custom Code End
     ################################################################################
 
-    phantom.custom_function(custom_function="is_if/unzip_file", parameters=parameters, name="unzip_file_2")
+    phantom.custom_function(custom_function="is_if/unzip_file", parameters=parameters, name="unzip_file_2", callback=playbook_detonate_file_1)
 
     return
 
 
 @phantom.playbook_block()
-def debug_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("debug_3() called")
+def playbook_detonate_file_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("playbook_detonate_file_1() called")
 
-    check_prompt_status_result_data = phantom.collect2(container=container, datapath=["check_prompt_status:action_result.data.0.response.password","check_prompt_status:action_result.parameter.context.artifact_id"], action_results=results)
-
-    check_prompt_status_result_item_0 = [item[0] for item in check_prompt_status_result_data]
-
-    parameters = []
-
-    parameters.append({
-        "input_1": check_prompt_status_result_item_0,
-        "input_2": None,
-        "input_3": None,
-        "input_4": None,
-        "input_5": None,
-        "input_6": None,
-        "input_7": None,
-        "input_8": None,
-        "input_9": None,
-        "input_10": None,
-    })
+    inputs = {}
 
     ################################################################################
     ## Custom Code Start
@@ -184,7 +170,80 @@ def debug_3(action=None, success=None, container=None, results=None, handle=None
     ## Custom Code End
     ################################################################################
 
-    phantom.custom_function(custom_function="community/debug", parameters=parameters, name="debug_3")
+    # call playbook "is_if/detonate_file", returns the playbook_run_id
+    playbook_run_id = phantom.playbook("is_if/detonate_file", container=container, name="playbook_detonate_file_1", callback=playbook_detonate_file_1_callback, inputs=inputs)
+
+    return
+
+
+@phantom.playbook_block()
+def playbook_detonate_file_1_callback(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("playbook_detonate_file_1_callback() called")
+
+    
+    # Downstream End block cannot be called directly, since execution will call on_finish automatically.
+    # Using placeholder callback function so child playbook is run synchronously.
+
+
+    return
+
+
+@phantom.playbook_block()
+def get_scheduler_artifact(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("get_scheduler_artifact() called")
+
+    # collect filtered artifact ids and results for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        logical_operator="and",
+        conditions=[
+            ["artifact:*.name", "==", "scheduled playbook"],
+            ["artifact:*.label", "==", "complete"]
+        ],
+        name="get_scheduler_artifact:condition_1",
+        delimiter=None)
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        artifact_update_4(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    return
+
+
+@phantom.playbook_block()
+def artifact_update_4(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("artifact_update_4() called")
+
+    filtered_artifact_0_data_get_scheduler_artifact = phantom.collect2(container=container, datapath=["filtered-data:get_scheduler_artifact:condition_1:artifact:*.id","filtered-data:get_scheduler_artifact:condition_1:artifact:*.id"])
+
+    parameters = []
+
+    # build parameters list for 'artifact_update_4' call
+    for filtered_artifact_0_item_get_scheduler_artifact in filtered_artifact_0_data_get_scheduler_artifact:
+        parameters.append({
+            "artifact_id": filtered_artifact_0_item_get_scheduler_artifact[0],
+            "name": None,
+            "label": "pending",
+            "severity": None,
+            "cef_field": None,
+            "cef_value": None,
+            "cef_data_type": None,
+            "tags": None,
+            "overwrite_tags": None,
+            "input_json": None,
+        })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.custom_function(custom_function="community/artifact_update", parameters=parameters, name="artifact_update_4")
 
     return
 
